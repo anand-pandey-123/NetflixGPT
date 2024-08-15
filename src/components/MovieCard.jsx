d138b9ef35110a9aee8f8199c40363b943877aa5
ANaNd@07 <ap123vapi@gmail.com>
2024-08-15 20:22:10 +0530

    commited

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { IMG_URL } from '../utils/constants'
 
 function MovieCard({poster}) {
   return (
-    <div className=' w-72 md:w-40 px-2 '>
+    <div className=' w-36 md:w-40 px-2 '>
         <img className='rounded-sm' src={IMG_URL + poster} alt="" />
     </div>
   )
